27a877e2a00a13e0a8d86c0415202ed7c144c8a1
ansondayo <tyhs007@hotmail.com>
2015-02-21 00:03:13 -0500

    Adding index.html and related files and folder

---
+++ js/detectmobilebrowser.js
@@ -0,0 +1,24 @@
+function detectmob() {
+    //alert('pass');
+    if (navigator.userAgent.match(/Android/i)
+            || navigator.userAgent.match(/webOS/i)
+            || navigator.userAgent.match(/iPhone/i)
+            || navigator.userAgent.match(/iPad/i)
+            || navigator.userAgent.match(/iPod/i)
+            || navigator.userAgent.match(/BlackBerry/i)
+            || navigator.userAgent.match(/Windows Phone/i)
+            ) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+if (detectmob()) {
+//    alert("is mobile");
+    window.location = 'http://anson.gweb.io/m/';
+} else {
+//    window.location = 'http://anson.gweb.io';
+//    alert("not mobile");
+}
